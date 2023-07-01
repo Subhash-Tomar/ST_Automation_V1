b7c7ef993ec19bc5e226c9f0bfd0901718cd7d7b
subhash tomar <subhashmca07@gmail.com>
2023-07-01 13:56:44 +0530

    add json file

--- RestAPI_Automation/src/test/java/cucumber/Runners/Testrunner.java
+++ RestAPI_Automation/src/test/java/cucumber/Runners/Testrunner.java
@@ -9,6 +9,7 @@ import io.restassured.RestAssured;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features="src/test/java/cucumber/Features",glue="cucumber/stepDefinitions",
+plugin = { "pretty", "json:target/cucumber-reports/Cucumber.json" },
 stepNotifications = true,
 monochrome = true)
 public class Testrunner
